400e76f468d986c3239043d53851e65c9443ef8f
Jachokoreto <jatan@u81z04s04.42kl.edu.my>
2022-01-10 09:55:06 +0800

    Add header files

---
+++ include/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jatan <jatan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/04 22:41:11 by jatan             #+#    #+#             */
+/*   Updated: 2021/08/08 20:38:09 by jatan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
+int		ft_strlen_2(const char *s);
+char	*get_next_line(int fd);
+
+#endif
---
+++ include/so_long.h
@@ -0,0 +1,8 @@
+#ifndef SO_LONG_H
+# define SO_LONG_H
+# include "get_next_line.h"
+# include "libft.h"
+
+void    load_map(void);
+
+#endif
