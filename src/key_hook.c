0cd7a8bd8551f6205482af90d8216453bdaaac63
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2022-01-15 07:31:07 +0800

    bloop

--- src/key_hook.c
+++ src/key_hook.c
@@ -1,17 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key_hook.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jatan <jatan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/15 07:30:30 by jatan             #+#    #+#             */
+/*   Updated: 2022/01/15 07:30:55 by jatan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 
 int    key_hook(int keycode, t_root *root)
 {
 	void	*blank;
-	// int		tmp_w;
-	// int		tmp_h;
-
-	printf("PLAYER INDEX: x=%d, y=%d\n", root->offset_x / 64, root->offset_y / 64);
+	int		x;
+	int		y;
 
 	if (keycode == 126 || keycode == 13)
 	{
 		if (root->map[(root->offset_y + 32) / 64 - 1][root->offset_x / 64] != '1')
-			root->offset_y -= 64;		
+			root->offset_y -= 64;
 	}
 	if (keycode == 125 || keycode == 1)
 	{
@@ -28,29 +38,22 @@ int    key_hook(int keycode, t_root *root)
 		if (root->map[root->offset_y / 64][(root->offset_x + 32) / 64 - 1] != '1')
 			root->offset_x -= 64;
 	}
-
 	if (keycode == 53)
 		exit_game(root);
-
 	if (root->map[root->offset_y / 64][root->offset_x / 64] == 'C')
 	{
 		root->map[root->offset_y / 64][root->offset_x / 64] = '0';
 		root->map_data[root->offset_y / 64][root->offset_x / 64][1] = NULL;
 		root->collectible_count--;
 	}
-
-	if (root->collectible_count == 0 && 
-			root->map[root->offset_y / 64][root->offset_x / 64] == 'E')
+	if (root->collectible_count == 0
+		&& root->map[root->offset_y / 64][root->offset_x / 64] == 'E')
 	{
 		printf("Exiting;");
 		exit_game(root);
 	}
-		
-
-	printf("collectible count: %d", root->collectible_count);
 	blank = mlx_new_image(root->mlx, WIN_SIZE, WIN_SIZE);
 	mlx_put_image_to_window(root->mlx, root->mlx_win, blank, 0 , 0);
-	// mlx_clear_window(root->mlx, root->mlx_win);
 	draw_map(root);
 	return (0);
 }
